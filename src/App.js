fb9f72151927aa931ffc567dcb6c4b51fe1252a8
Abhishek-st <ankit2632000@gmail.com>
2021-07-16 13:14:13 +0530

    added hooks and working logic for editor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Code from './component/Code'
+import MyLocalStorage from './hooks/MyLocalStorage'
 
 function App() {
+  const [html, setHtml] = MyLocalStorage('html', '')
+  const [css, setCss] = MyLocalStorage('css', '')
+  const [js, setJs] = MyLocalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    }, 250)
+
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
+
   return (
-    <div>
-      <div className="pane top-pane"> 
-          <Code />
-          <Code />
-          <Code />
+    <>
+      <div className="pane top-pane">
+        <Code
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Code
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Code
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
       </div>
       <div className="pane">
         <iframe
-        title="output"
-        sandbox="allow-scripts"
-        width="100%"
-        height="100%"
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
         />
       </div>
-    </div>
+    </>
   )
 }
 
-export default App
+export default App;
 
 
